d657e4fd38e9b607139548d8472a2c23b3943aaa
Diego Vàsquez <Alejandrovasq0803gmail.com>
2026-04-12 18:00:31 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ios_trye_compilate
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ios_trye_compilate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ios_trye_compilate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
